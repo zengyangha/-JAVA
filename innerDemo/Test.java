5f54a977b0fe3d3b37ae76c300470844098e862e
Yang <153696478+zengyangha@users.noreply.github.com>
2024-04-23 21:37:58 +0800

    对内部类的初步认知

---
+++ innerDemo/Test.java
@@ -0,0 +1,30 @@
+package demo_test.innerDemo;
+
+public class Test {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+
+        Outer.Iner inner=new Outer().new Iner();
+
+        Outer.Iner2 inner2=new Outer.Iner2();
+
+        outer.test();//间接访问
+        System.out.println();
+
+        inner.inerTest();//直接访问
+        System.out.println();
+
+        inner.show();
+
+        //System.out.println(inner.age);  报错
+
+        System.out.println();
+        inner2.show();
+
+        outer.show();
+
+        //还有一种内部类叫匿名内部类，很重要
+
+    }
+
+}
